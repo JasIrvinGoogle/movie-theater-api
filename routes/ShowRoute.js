3a68236d00f8609005ac353764e9d5eaecf2e7e4
JasIrvin <jasmineirvin@google.com>
2022-11-14 17:37:04 -0500

    started setting up routes - unsure if this is correct

--- routes/ShowRoute.js
+++ routes/ShowRoute.js
@@ -1,3 +1,10 @@
 const express = require('express')
+const app = express()
+const Show = require('../../scooter-project/src/Show')
 const router = express.Router()
+
+app.get('/shows', async (request, response) => {
+    const shows = await Show.findAll(); 
+    response.json(shows);
+})
 
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,2 +1,9 @@
 const express = require('express')
+const app = express()
+const User = require('../../scooter-project/src/User')
 const router = express.Router()
+
+app.get('/users', async (request, response) => {
+    const users = await User.findAll(); 
+    response.json(users);
+})
