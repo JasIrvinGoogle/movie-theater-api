1ef03b5b6db0ab4b1b9f62e5fc6f430445020471
JasIrvin <jasmineirvin@google.com>
2022-11-17 11:24:55 -0500

    updates to user routes

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,18 +4,18 @@ const User = require('../models/User')
 const router = express.Router()
 
 //get all users from user DB
-app.get('/users', async (request, response) => {
+router.get('/users', async (request, response) => {
     const users = await User.findAll(); 
     response.json(users);
 })
 
 //get a specific user from user DB
-app.get('/users/:id', async (request, response) => {
+router.get('/users/:id', async (request, response) => {
     const user = await User.findByPk(request.params.id);
     response.json(user);
 })
 
 //get all shows watched by a user(user id in req.params)
-app.get()
+
 
 //put update and add a show if user has watched it
